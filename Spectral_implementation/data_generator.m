4eeb5805009f5b5cfc04ed149ccd3f10d570a2b1
Rishabh Deshmukh <rishabh.deshmukh@csa.iisc.ernet.in>
2018-01-07 15:21:48 +0530

    Spectral Implementation Completed

---
+++ Spectral_implementation/data_generator.m
@@ -0,0 +1,48 @@
+% N = 2^10 * 5;
+% d = 5;
+% K = 2^8; %Number of groups
+% conc_crcl = 64;
+N = 2^13;
+d = 2;
+K = 2^9; %Number of groups
+% n = N/K; % size of each group
+conc_crcl = 2^5;
+mn = 500;
+mu = ones([1 ,d])*mn;
+R = cell(conc_crcl, 1);
+temp=0;
+
+for i = 1:conc_crcl
+    R{i} = zeros(2,1);
+    R{i}(1) = temp + randi([8,12]);
+    R{i}(2) = R{i}(1) + randi([1,2]);
+    temp = R{i}(2);
+end
+
+data = zeros(N,d);
+cnt1=1;
+pnts_in_each = N/conc_crcl;
+for i = 1:conc_crcl
+    ri_1 = R{i}(1);
+    ri_2 = R{i}(2);
+    sig = 2*ri_2;
+    cnt2 = pnts_in_each;
+    while cnt2>0
+        X = abs(normrnd(mu, sig, [1,d]));
+        if isValid(X, ri_1, ri_2, mu)
+            data(cnt1, :) = X;
+            cnt1 = cnt1 + 1;
+            cnt2 = cnt2 - 1;
+        end
+    end
+end
+
+input.data = data;
+input.N = N;
+input.d = d;
+
+plot(data(:,1), data(:,2), '.r');
+
+
+
+
---
+++ Spectral_implementation/spectral_modularity.m
@@ -0,0 +1,44 @@
+function [grp_cell, grp_size] = spectral_modularity(K, lambda, distanceMat)
+    
+    degree_vect = sum(distanceMat,2);
+    degree_mat = degree_vect * degree_vect';
+    m = sum(sum(distanceMat))/2;
+    data_len = size(distanceMat, 1);
+    Mat = distanceMat - degree_mat/(2*m) - lambda*ones(data_len);
+    tic
+    fprintf('Eigen Decomposition Initiated\n')
+    [Evec, ~] = eig(Mat);
+    fprintf('Eigen Decomposition completed\n')
+    toc
+    
+    D = zeros(data_len, K);
+    
+    fprintf('Selecting eigen vectors for K-Means\n')
+    eig_vect_cnt=1;
+    
+    for i=1:K
+        eig_vec = Evec(:, eig_vect_cnt);
+        while( (sum(eig_vec>=0) == 0 || sum(eig_vec<0) == 0) )
+            eig_vect_cnt = eig_vect_cnt+1;
+            eig_vec = Evec(:, eig_vect_cnt);
+        end
+        D(:, i) = eig_vec;
+        eig_vect_cnt = eig_vect_cnt+1;
+    end
+    
+    fprintf('Eigen Vectors for KMeans has been selected\n\n')
+    
+    tic
+    fprintf('Performing Kmeans Clustering\n')    
+    IDX = kmeans(D, K);
+    fprintf('Kmeans Clustering completed\n')
+    toc
+    grp_cell = cell(K,1);
+    grp_size = zeros(K,1);
+    
+    for i=1:K
+        grp_cell{i} = find(IDX==i);
+        grp_size(i) = length(grp_cell{i});
+    end
+   
+end
